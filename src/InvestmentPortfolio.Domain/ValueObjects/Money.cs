74216f0837568995e90e07f9c50d493a9e4f8fdf
Gabriel Batista <gabriel_batista@outlook.com>
2025-07-02 13:21:36 +0000

    chore: add `XML documentation comments`

--- src/InvestmentPortfolio.Domain/ValueObjects/Money.cs
+++ src/InvestmentPortfolio.Domain/ValueObjects/Money.cs
@@ -1,9 +1,27 @@
 ﻿namespace InvestmentPortfolio.Domain.ValueObjects;
 
+/// <summary>
+/// Represents a monetary value with a specific currency.
+/// This class encapsulates the amount and currency, ensuring that the amount is non-negative 
+/// and has at most two decimal places, and that the currency is valid (currently only "BRL" is supported).
+/// <br /><br />It provides methods to create instances of Money, check if the amount is zero, and validate the amount and currency values.
+/// <br /><br />The class is immutable, meaning once an instance is created, its state cannot be changed.
+/// </summary>
 public class Money
 {
+    /// <summary>
+    /// Gets the amount of money.
+    /// <br />The amount is non-negative and has at most two decimal places.
+    /// </summary>
     public decimal Amount { get; }
+    /// <summary>
+    /// Gets the currency of the money.
+    /// <br />Currently, only "BRL" (Brazilian Real) is supported.
+    /// </summary>
     public string Currency { get; }
+    /// <summary>
+    /// Checks if the amount of money is zero.
+    /// </summary>
     public bool IsZero() => Amount == 0;
 
     private Money(decimal amount, string currency)
@@ -42,6 +60,12 @@ public class Money
         }
     }
 
+    ///<summary>
+    /// Factory method Creates a new instance of Money with the specified amount and currency.
+    /// <param name="amount">The amount of money, must be non-negative and have at most two decimal places.</param>
+    /// <param name="currency">The currency of the money, must be "BRL".</param>
+    /// <returns>A new instance of Money.</returns>
+    ///</summary>
     public static Money Create(decimal amount, string currency)
     {
         ValidateAmount(amount);
@@ -49,6 +73,11 @@ public class Money
         return new Money(amount, currency);
     }
 
+    /// <summary>
+    /// Factory method to create a Money instance with zero amount in the specified currency.
+    /// </summary>
+    /// <param name="currency"></param>
+    /// <returns></returns>
     public static Money Zero(string currency = "BRL")
     {
         return new Money(0, currency);
